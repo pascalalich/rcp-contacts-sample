b5a8fc4401921ad28aa64302174512de5f6d7fa7
Pascal Alich <pascal.alich@gmx.de>
2012-07-27 16:37:44 +0200

    Initial extension commit
    Callback interfaces created, plugins initially created

---
+++ com.zuehlke.contacts.ui/src/com/zuehlke/contacts/ui/addressCheck/IAddressCheck.java
@@ -0,0 +1,19 @@
+package com.zuehlke.contacts.ui.addressCheck;
+
+import com.zuehlke.contacts.service.dto.Address;
+
+/**
+ * Callback for address check extensions.
+ */
+public interface IAddressCheck {
+
+	/**
+	 * Checks the address.
+	 * 
+	 * @param address
+	 *            the address to check (mandatory)
+	 * @return the check result
+	 */
+	AddressCheckResult check(Address address);
+
+}
