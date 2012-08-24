eb54f7cd5ff5a78d3338bf58d7720cbaff76b6be
Pascal Alich <pascal.alich@gmx.de>
2012-08-25 01:11:52 +0200

    Google Maps Address Check working

--- com.zuehlke.contacts.ui.addressCheck.maps/src/com/zuehlke/contacts/ui/addresscheck/maps/Activator.java
+++ com.zuehlke.contacts.ui.addressCheck.maps/src/com/zuehlke/contacts/ui/addresscheck/maps/Activator.java
@@ -1,6 +1,12 @@
 package com.zuehlke.contacts.ui.addresscheck.maps;
 
+import org.eclipse.core.internal.registry.osgi.OSGIUtils;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -13,7 +19,7 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -22,16 +28,23 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
+		registerServices();
 		plugin = this;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
@@ -40,11 +53,42 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
 		return plugin;
 	}
 
+	/**
+	 * Registers OSGi services.
+	 */
+	private void registerServices() {
+		// Defer until workbench is running
+		Job job = new Job("Register services for "
+				+ getBundle().getSymbolicName()) {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				BundleContext bundleContext = getBundle().getBundleContext();
+				while (OSGIUtils.getDefault()
+						.getBundle("com.zuehlke.contacts.ui").getState() != Bundle.ACTIVE) {
+					try {
+						Thread.sleep(50);
+					} catch (InterruptedException e) {
+						throw new IllegalStateException(
+								"Interrupted while waiting for workbench.", e);
+					}
+				}
+				// fully qualified to defer startup of UI bundle
+				bundleContext
+						.registerService(
+								com.zuehlke.contacts.ui.addresscheck.IAddressCheck.class,
+								new MapsAddressCheck(), null);
+
+				return Status.OK_STATUS;
+			}
+		};
+		job.setSystem(true);
+		job.schedule();
+	}
 }
